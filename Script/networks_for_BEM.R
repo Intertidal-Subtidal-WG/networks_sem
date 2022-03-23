3b78fe3631f2534dc479f3a84bbf5cf493eaf878
jbburant <jburant@uoguelph.ca>
2022-03-22 21:29:55 -0400

    script updates

--- Script/networks_for_BEM.R
+++ Script/networks_for_BEM.R
@@ -70,7 +70,12 @@ V(combined_graph)$interaction_type <- interactions_comb2$interactionTypeName
 ## (i.e., how many connections are made to each node?)
 V(combined_graph)$degree <- igraph::degree(combined_graph, 
                                            ## scale by 0.1
+                                           mode = "all")
+## use the degree as a weighting variable for plotting nodes
+V(combined_graph)$weight <- igraph::degree(combined_graph, 
+                                           ## scale by 0.1
                                            mode = "all") * 0.1
+
 ## inspect edge (link) attributes
 # edge_attr(combined_graph)
 edge_attr_names(combined_graph)
@@ -84,9 +89,9 @@ tidy_combined_g %>%
   mutate(zone = factor(zone, 
                        levels = c("Subtidal", "Both", "Intertidal"))) %>% 
   ggraph(layout = "kk") + 
-  geom_node_point(aes(fill = zone, size = degree), shape = 21) +
-  geom_edge_arc(aes(linetype = interactionTypeName), 
-                alpha = 0.5, width = 0.5) + 
+  geom_node_point(aes(fill = zone, size = weight), shape = 21) +
+  geom_edge_link(aes(linetype = interactionTypeName),
+                 alpha = 0.5, width = 0.5) +
   scale_fill_manual(values = net_pal, name = "Zone") +
   # scale_fill_brewer(palette = "Dark2", name = "Zone") + 
   # geom_node_text(aes(label = name), colour = 'white', vjust = 0.4) +
@@ -127,11 +132,11 @@ p.comb <- layout_combined2 %>%
   mutate(zone = factor(zone, 
                        levels = c("Subtidal", "Both", "Intertidal"))) %>% 
   ggraph() + 
-  # geom_edge_arc(aes(linetype = interactionTypeName), 
-  #               alpha = 0.5, width = 0.5) + 
-  geom_edge_hive(aes(linetype = interactionTypeName), 
+  # geom_edge_hive(aes(linetype = interactionTypeName),
+  #                colour = "grey30", alpha = 0.5, width = 0.5) +
+  geom_edge_link(aes(linetype = interactionTypeName), 
                  colour = "grey30", alpha = 0.5, width = 0.5) + 
-  geom_node_point(aes(fill = zone, size = degree), shape = 21) +
+  geom_node_point(aes(fill = zone, size = weight), shape = 21) +
   scale_fill_manual(values = net_pal, name = "Zone") + 
   # scale_fill_brewer(palette = "Dark2", name = "Zone") +
   # geom_node_text(aes(label = name), colour = 'white', vjust = 0.4) +
@@ -144,7 +149,7 @@ p.comb <- layout_combined2 %>%
         legend.box = "vertical")
 
 ggsave(filename = "Plots/network_combined.png", plot = p.comb, 
-       width = 6, heigh = 4.5, units = "in", dpi = "retina")
+       width = 4, heigh = 6.5, units = "in", dpi = "retina")
 
 
 # subset interactions by zone ---------------------------------------------
@@ -168,14 +173,25 @@ interactions_comb2 %>%
 ## How many subtidal/both interactions should there be?
 ## Both+Both + Both+Subtidal + Subtidal+Both + Subtidal+Subtidal
 ## 36 + 49 + 80 + 126 = 291 <--- nrow(interactions_sub) below
-## 
+## 391 / 548 = 53% of interactions set
+##
 ## How many intertidal/both interactions should there be?
 ## Both+Both + Both+Intertidal + Intertidal+Both + Intertidal+Intertidal
 ## 36 + 31 + 39 + 53 = 159 <--- nrow(interactions_int) below
+## 159 / 548 = 29% of interactions set
 ##
 ## How many interactions span the subtidal and intertidal?
 ## Intertidal+Subtidal + Subtidal+Intertidal
-## 53 + 81 = 134
+## 53 + 81 = 134 
+## 134 / 548 = 24% of interactions set
+## ^^ this is the percentage missed when you treat the subtidal
+## and intertidal as separate networks! many of these interactions
+## are likely missed in the observational data because of they involve
+## mobile organisms *can* be found in both habitats but only enter
+## intertidal at high tide (hence, missed in sampling protocol)
+## for a "observed" Subtidal organism to interact with an "observed"
+## Intertidal organism, they must move into the other zone (so should
+## technically be classified as "Both")
 
 ## subset the interactions to include only those that occur between
 ## species found in the subtidal (or both)
@@ -217,8 +233,13 @@ V(sub_graph)$interaction_type <- interactions_sub$interactionTypeName
 ## calculate and add an attribute for degree 
 ## (i.e., how many connections are made to each node?)
 V(sub_graph)$degree <- igraph::degree(sub_graph, 
-                                           ## scale by 0.1
-                                           mode = "all") * 0.1
+                                      ## scale by 0.1
+                                      mode = "all")
+## use the degree as a weighting variable for plotting nodes
+V(sub_graph)$weight <- igraph::degree(sub_graph, 
+                                      ## scale by 0.1
+                                      mode = "all") * 0.1
+
 ## inspect edge (link) attributes
 # edge_attr(combined_graph)
 edge_attr_names(sub_graph)
@@ -232,9 +253,9 @@ tidy_sub_g %>%
   mutate(zone = factor(zone, 
                        levels = c("Subtidal", "Both", "Intertidal"))) %>% 
   ggraph(layout = "kk") + 
-  geom_node_point(aes(fill = zone, size = degree), shape = 21) +
-  geom_edge_arc(aes(linetype = interactionTypeName), 
-                alpha = 0.5, width = 0.5) + 
+  geom_node_point(aes(fill = zone, size = weight), shape = 21) +
+  geom_edge_link(aes(linetype = interactionTypeName), 
+                 alpha = 0.5, width = 0.5) + 
   scale_fill_manual(values = net_pal, name = "Zone") + 
   # scale_fill_brewer(palette = "Dark2", name = "Zone") + 
   # geom_node_text(aes(label = name), colour = 'white', vjust = 0.4) +
@@ -273,11 +294,11 @@ p.sub <- layout_sub2 %>%
   mutate(zone = factor(zone, 
                        levels = c("Subtidal", "Both", "Intertidal"))) %>% 
   ggraph() + 
-  # geom_edge_arc(aes(linetype = interactionTypeName), 
-  #               alpha = 0.5, width = 0.5) + 
-  geom_edge_hive(aes(linetype = interactionTypeName), 
+  # geom_edge_hive(aes(linetype = interactionTypeName),
+  #                colour = "grey30", alpha = 0.5, width = 0.5) +
+  geom_edge_link(aes(linetype = interactionTypeName), 
                  colour = "grey30", alpha = 0.5, width = 0.5) + 
-  geom_node_point(aes(fill = zone, size = degree), shape = 21) +
+  geom_node_point(aes(fill = zone, size = weight), shape = 21) +
   scale_fill_manual(values = net_pal, name = "Zone") + 
   # scale_fill_brewer(palette = "Dark2", name = "Zone") +
   # geom_node_text(aes(label = name), colour = 'white', vjust = 0.4) +
@@ -289,7 +310,7 @@ p.sub <- layout_sub2 %>%
         legend.box = "vertical")
 
 ggsave(filename = "Plots/network_subtidal.png", plot = p.sub, 
-       width = 6, heigh = 4.5, units = "in", dpi = "retina")
+       width = 4, heigh = 6.5, units = "in", dpi = "retina")
 
 
 # intertidal interaction network ------------------------------------------
@@ -319,7 +340,12 @@ V(int_graph)$interaction_type <- interactions_int$interactionTypeName
 ## (i.e., how many connections are made to each node?)
 V(int_graph)$degree <- igraph::degree(int_graph, 
                                       ## scale by 0.1
+                                      mode = "all")
+## use the degree as a weighting variable for plotting nodes
+V(int_graph)$weight <- igraph::degree(int_graph, 
+                                      ## scale by 0.1
                                       mode = "all") * 0.1
+
 ## inspect edge (link) attributes
 # edge_attr(combined_graph)
 edge_attr_names(int_graph)
@@ -333,9 +359,9 @@ tidy_int_g %>%
   mutate(zone = factor(zone, 
                        levels = c("Subtidal", "Both", "Intertidal"))) %>% 
   ggraph(layout = "kk") + 
-  geom_node_point(aes(fill = zone, size = degree), shape = 21) +
-  geom_edge_arc(aes(linetype = interactionTypeName), 
-                alpha = 0.5, width = 0.5) + 
+  geom_node_point(aes(fill = zone, size = weight), shape = 21) +
+  geom_edge_link(aes(linetype = interactionTypeName), 
+                 alpha = 0.5, width = 0.5) + 
   scale_fill_manual(values = net_pal[2:3], name = "Zone") + 
   # scale_fill_brewer(palette = "Dark2", name = "Zone") + 
   # geom_node_text(aes(label = name), colour = 'white', vjust = 0.4) +
@@ -374,11 +400,11 @@ p.int <- layout_int2 %>%
   mutate(zone = factor(zone, 
                        levels = c("Subtidal", "Both", "Intertidal"))) %>% 
   ggraph() + 
-  # geom_edge_arc(aes(linetype = interactionTypeName), 
-  #               alpha = 0.5, width = 0.5) + 
-  geom_edge_hive(aes(linetype = interactionTypeName), 
+  # geom_edge_hive(aes(linetype = interactionTypeName),
+  #                colour = "grey30", alpha = 0.5, width = 0.5) +
+  geom_edge_link(aes(linetype = interactionTypeName), 
                  colour = "grey30", alpha = 0.5, width = 0.5) + 
-  geom_node_point(aes(fill = zone, size = degree), shape = 21) +
+  geom_node_point(aes(fill = zone, size = weight), shape = 21) +
   scale_fill_manual(values = net_pal[2:3], name = "Zone") + 
   # scale_fill_brewer(palette = "Dark2", name = "Zone") +
   # geom_node_text(aes(label = name), colour = 'white', vjust = 0.4) +
@@ -390,10 +416,57 @@ p.int <- layout_int2 %>%
         legend.box = "vertical")
 
 ggsave(filename = "Plots/network_intertidal.png", plot = p.int, 
-       width = 6, heigh = 4.5, units = "in", dpi = "retina")
+       width = 4, heigh = 6.5, units = "in", dpi = "retina")
 
 
 # plot all three networks together ----------------------------------------
 
 ## using patchwork
 (p.sub + p.int) / p.comb
+
+
+# representation of Appledore species in networks -------------------------
+
+## how many unique species are in the species list?
+sp_zone2 %>% 
+  group_by(sp_zone) %>% 
+  summarise(n_sp = n_distinct(organism))
+# sp_zone     n_sp
+# 1 Both          25
+# 2 Intertidal    35
+# 3 Subtidal      84
+
+## COMMENT:
+## so there are:
+## 144 total species (25+25+84)
+## 109 subtidal species (25+84)
+## 60 intertidal species (25+35)
+
+## how many species are in each network?
+## combined
+n_distinct(V(combined_graph)$name) ## [1] 101
+101/144 ## [1] 0.7013889
+## COMMENT:
+## our interactions dataset include ~70% of species found at Appledore
+
+## subtidal
+n_distinct(V(sub_graph)$name) ## [1] 71
+71/109 ## [1] 0.6513761
+## COMMENT:
+## subtidal interactions include ~65% of species found in subtidal
+
+## intertidal
+n_distinct(V(int_graph)$name) ## [1] 45
+45/60 ## [1] 0.75
+## intertidal interactions include 75% of species found in intertidal
+
+
+## other goals?
+## create a graph of only interactions including a species found in 
+## both zones (excluding species that only interact with a species 
+## that isn't found in both zones)
+
+
+# node centrality ---------------------------------------------------------
+
+tidy_combined_g %>% mutate(centrality = centrality_authority())
